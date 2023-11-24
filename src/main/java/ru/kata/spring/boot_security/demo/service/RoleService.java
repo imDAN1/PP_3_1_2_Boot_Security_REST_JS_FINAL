097836f9185753bb548f11fcc3340ba592116383
Diman <dimanozdrin1999@gmail.com>
2023-11-24 12:35:53 +0300

    1

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -5,7 +5,6 @@ import ru.kata.spring.boot_security.demo.model.Role;
 
 import java.util.List;
 
-@Service
 public interface RoleService {
     List<Role> getAllRoles();
 }
--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -3,13 +3,10 @@ package ru.kata.spring.boot_security.demo.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
 
-@Service
 public interface UserService extends UserDetailsService {
     List<User> getAllUsers();
     void createUser(User user);
